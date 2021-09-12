3636b28a82dc787a4c593fa8c64114e91157098e
NurulIslam-EEE <ni163034@gmail.com>
2021-09-13 04:29:50 +0600

    details added

--- js/app.js
+++ js/app.js
@@ -1,34 +1,37 @@
 const loadProducts = () => {
   const url = 'https://fakestoreapi.com/products';
   fetch(url)
-    .then((response) => response.json())
-    .then((data) => showProducts(data));
+    .then(response => response.json())
+    .then(data => showProducts(data))
 };
 loadProducts();
 
-// show all product in UI 
+// ------------show all product in UI -----------
+
 const showProducts = (products) => {
   console.log(products);
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+    const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
       <div>
-    <img class="product-image" src=${product.image}></img>
+    <img class="product-image" src=${image}></img>
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
+      <span>${product.rating.rate} (${product.rating.count})</span>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <button onclick="loadProductsById(${product.id})" id="details-btn" class="btn btn-danger">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
 };
 
-// add to cart 
+// --------------add to cart -------------
+
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
@@ -38,14 +41,17 @@ const addToCart = (id, price) => {
   updateTotal();
   document.getElementById("total-Products").innerText = count;
 };
-// getting inner text   ok
+
+// -------------getting inner text ----------------  
+
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
   const converted = parseFloat(element);
   return converted;
 };
 
-// main price update function     ok
+// ----------------- price update function ------------
+
 const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
@@ -53,12 +59,14 @@ const updatePrice = (id, value) => {
   document.getElementById(id).innerText = total;
 };
 
-// set innerText function                    ok
+// --------------------set innerText function ----------------
+
 const setInnerText = (id, value) => {
   document.getElementById(id).innerText = value;
 };
 
-// update delivery charge and total Tax     ok
+// -----------------update delivery charge and total Tax ---------------
+
 const updateTaxAndCharge = () => {
   const priceConverted = getInputValue("price");
 
@@ -76,10 +84,42 @@ const updateTaxAndCharge = () => {
   }
 };
 
-//grandTotal update function
+//----------------grandTotal update function-------------
+
 const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
   document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
+// load products by id
+const loadProductsById = (id) => {
+  const url = `https://fakestoreapi.com/products/${id}`;
+  fetch(url)
+    .then(response => response.json())
+    .then(data => showDetails(data))
+}
+// ----------show details---------
+const showDetails = (product) => {
+
+  const parent = document.getElementById('details');
+  const modal = document.createElement('div');
+  parent.innerHTML = '';
+  modal.innerHTML = `
+  <div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="${product.image}" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">${product.title}</h5>
+        <p class="card-text">${product.description}</p>
+       
+      </div>
+    </div>
+  </div>
+</div>
+`;
+  parent.appendChild(modal);
+}
